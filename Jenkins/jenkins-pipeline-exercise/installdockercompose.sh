3229bd58b86be46a83374d369432aeb81746caee
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2022-12-07 15:53:05 +0000

    Jenkins

---
+++ Jenkins/jenkins-pipeline-exercise/installdockercompose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install -y jq
+# set which version to download (latest)
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# download to /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# make the file executable
+sudo chmod +x /usr/local/bin/docker-compose
